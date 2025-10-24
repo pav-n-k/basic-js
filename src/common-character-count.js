28121e692fb08c94fe7130bad10ab73d74d544dc
pavel <pav-n-k@yandex.ru>
2025-10-25 01:09:34 +0300

    feat: Common-character-count test will pass

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,9 +12,29 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  if (s1 === '' || s2 === '') return 0;
+  const getSObj = (s) => s.split('').reduce((obj, el) => {
+    if (!obj[el]) {
+      obj[el] = 1;
+    } else {
+      obj[el] += 1;
+    }
+    return obj;
+  }, {});
+
+  const obj1 = getSObj(s1);
+  const obj2 = getSObj(s2);
+
+  let result = 0;
+
+  Object.keys(obj1).forEach((el) => {
+    if (obj1[el] && obj2[el]) {
+      result += Math.min(obj1[el], obj2[el]);
+    }
+  });
+
+  return result;
 }
 
 module.exports = {
