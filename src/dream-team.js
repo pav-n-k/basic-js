d84274c661bb77a6b8e463b192b708711fec89ce
pavel <pav-n-k@yandex.ru>
2025-10-25 16:56:44 +0300

    feat: Dream-team test will pass

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const result = members
+    .filter((member) => typeof member === 'string')
+    .map((member) => member.trim().toUpperCase())
+    .map((member) => member[0])
+    .sort()
+    .join('');
+  return result;
 }
 
 module.exports = {
